2d170b91d44bce9f86d7348516b615ad5fa78d1b
nirbhaybheda <nirbhay.eighty@gmail.com>
2017-05-26 01:58:57 +0530

    basic setup

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rnGeneratorApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the rnGeneratorApp
+ */
+angular.module('rnGeneratorApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rnGeneratorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the rnGeneratorApp
+ */
+angular.module('rnGeneratorApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
